2ca4c14cf0e5aa8be4e25c3f7a68bafecbede101
begaias <a.b.k.5025@gmail.com>
2017-05-01 19:23:18 +0600

    View Login

--- app/src/main/java/com/example/beka/loggin/Main2Activity.java
+++ app/src/main/java/com/example/beka/loggin/Main2Activity.java
@@ -1,13 +1,32 @@
 package com.example.beka.loggin;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
-public class Main2Activity extends AppCompatActivity {
+public class Main2Activity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        ImageButton imageButton = (ImageButton) findViewById(R.id.iB);
+        imageButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.iB:
+                Intent intent = new Intent(Main2Activity.this, Settings.class);
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
     }
 }

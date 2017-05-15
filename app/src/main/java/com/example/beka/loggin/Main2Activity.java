ca8b6a905d32c9dfc2388fc740d77a78f801f56e
begaias <a.b.k.5025@gmail.com>
2017-05-15 17:02:22 +0600

    Date

--- app/src/main/java/com/example/beka/loggin/Main2Activity.java
+++ app/src/main/java/com/example/beka/loggin/Main2Activity.java
@@ -7,7 +7,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 
-public class Main2Activity extends AppCompatActivity implements View.OnClickListener{
+public class Main2Activity extends AppCompatActivity implements View.OnClickListener {
+
+    private View v;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +26,10 @@ public class Main2Activity extends AppCompatActivity implements View.OnClickList
             case R.id.iB:
                 Intent intent = new Intent(Main2Activity.this, Settings.class);
                 startActivity(intent);
+                break;
+            case R.id.btnDate:
+                Intent intent1 = new Intent(Main2Activity.this, Date.class);
+                startActivity(intent1);
                 break;
             default:
                 break;

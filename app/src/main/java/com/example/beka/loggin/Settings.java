ca9650f0f2ccaa78a4a3515ac7c4585677c921f1
begaias <a.b.k.5025@gmail.com>
2017-05-25 19:39:38 +0600

    Добавила функциональность кнопкам radiobutton

--- app/src/main/java/com/example/beka/loggin/Settings.java
+++ app/src/main/java/com/example/beka/loggin/Settings.java
@@ -2,6 +2,11 @@ package com.example.beka.loggin;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.TextView;
+
+import static com.example.beka.loggin.R.id.radioButton;
 
 public class Settings extends AppCompatActivity {
 
@@ -9,5 +14,24 @@ public class Settings extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+    }
+
+    public void onRadioButtonClicked(View view) {
+
+        boolean checked = ((RadioButton) view).isChecked();
+        TextView selection = (TextView) findViewById(R.id.selection);
+        switch(view.getId()) {
+            case R.id.radioButton:
+                if (checked){
+                    selection.setText("Уведомления включены");
+                }
+                break;
+            case R.id.radioButton2:
+                if (checked){
+                    selection.setText("Уведомления отключены");
+                }
+                break;
+        }
     }
 }
